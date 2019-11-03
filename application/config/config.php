c93c45a5ef54de509ef2082f367be6d2de9ae1e3
willianrefky <alvianbima29@gmail.com>
2019-11-03 09:43:25 +0700

    merubah configurasi pada config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/toko_robby/';
+$config['base_url'] = 'http://localhost/SI_Toko_Robby/';
 
 /*
 |--------------------------------------------------------------------------
