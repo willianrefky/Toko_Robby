398c243731f2f4de07769a736f227c00435c4093
AriefRachman99 <mariefrachmanm@gmail.com>
2019-12-04 14:46:15 +0700

    login show passsword

--- application/views/login/v_login.php
+++ application/views/login/v_login.php
@@ -12,7 +12,7 @@
 						<input type="text" class="form-control" placeholder="Username" name="username" required="">
 					</div>
 					<div class="form-group has-feedback">
-						<input type="password" class="form-control" placeholder="Password" name="password" required="">
+						<input type="password" class="form-control" placeholder="Password" name="password" id="password" required="">
 					</div>
 					<?php 
 						// Cek apakah terdapat session bernama message
@@ -21,7 +21,7 @@
 						}
 					?>
 					<div class="row">
-						<div class="col-8"></div>
+						<div class="col-8"><input type="checkbox" onclick="showPassword()"> Show Password</div>
 						<div class="col-4">
 							<input type="submit" class="btn btn-primary btn-block" value="Login">
 						</div>
@@ -37,3 +37,14 @@
     	</div>
   	</footer>
 </body>
+<script>
+	function showPassword() {
+		var getInput = document.getElementById("password");
+		if (getInput.type === "password") {
+			getInput.type = "text";
+		} 
+		else {
+			getInput.type = "password";
+		}
+	}
+</script>
